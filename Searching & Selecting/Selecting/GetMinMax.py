94536a180e663f792723852add05993c2a868d40
coachgibbo <shareefabdelal@gmail.com>
2022-10-18 21:06:29 +1100

    Add algorithm to optimally get the min and the max in the same pass

--- Searching & Selecting/Selecting/GetMinMax.py
+++ Searching & Selecting/Selecting/GetMinMax.py
@@ -17,8 +17,35 @@ def getMax(array):
     return maximum
 
 
+def getOptimizedMinMax(array):
+    n = len(array)
+    if n % 2 != 0:
+        minimum, maximum, start_index = array[1], array[1], 1
+    else:
+        if array[0] < array[1]:
+            minimum, maximum, start_index = array[0], array[1], 2
+        else:
+            minimum, maximum, start_index = array[1], array[0], 2
+
+    for i in range(start_index, n, 2):
+        if array[i] < array[i+1]:
+            if array[i] < minimum:
+                minimum = array[i]
+            if array[i+1] > maximum:
+                maximum = array[i+1]
+        else:
+            if array[i] > maximum:
+                maximum = array[i]
+            if array[i+1] < minimum:
+                minimum = array[i+1]
+
+    return minimum, maximum
+
+
 if __name__ == "__main__":
     test_input = TestData.generate_list_of_ints(1, 200, 20)
     print("Test Input: " + str(test_input))
     print("Min: " + str(getMin(test_input)))
     print("Max: " + str(getMax(test_input)))
+    optMinMax = getOptimizedMinMax(test_input)
+    print(str.format("Optimized Min: {} and Optimized Max: {}", optMinMax[0], optMinMax[1]))
